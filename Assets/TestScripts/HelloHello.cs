aca92ad90b077738602e9bed8945d8ceee6ea077
Kaiden Richardson <kaidenr@shaw.ca>
2023-06-24 13:23:14 -0700

    Made it so colors change a bunch.

--- Assets/TestScripts/HelloHello.cs
+++ Assets/TestScripts/HelloHello.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class HelloHello : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Renderer rend;
+
     void Start()
     {
         Debug.Log("Hello World");
         Debug.Log("Scripting change");
+        rend = GetComponent<Renderer>();
+
+        StartCoroutine(ChangeColor());
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<Color> colors = new List<Color>() {Color.red, Color.green, Color.blue};
+    private WaitForSeconds wait1s = new WaitForSeconds(1f);
+
+    private IEnumerator ChangeColor()
     {
-        
+        int color = 0;
+        while(true)
+        {
+            if(color >= colors.Count)
+            {
+                color = 0;
+            }
+
+            rend.material.SetColor("_Color", colors[color]);
+            color++;
+
+            yield return wait1s;
+        }
     }
 }
